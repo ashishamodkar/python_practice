9635835f06258d129e2fb12b3d9dd8beb71c498e
ashishamodkar <aashishamodkar123@gmail.com>
2019-12-22 18:23:53 +0530

    program for try Exception Handling

---
+++ try exeption program.py
@@ -0,0 +1,10 @@
+num1=input("Enter the num1")
+num2=input("Enter the num2")
+try:
+    print("The sum of these two number is : ",
+          int(num1)+ int(num2))
+except Exception  as e:
+    print(e)
+
+
+print("This Line is Important")
