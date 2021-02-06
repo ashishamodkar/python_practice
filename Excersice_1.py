8d445e6e2d4ef84b5d9dd6e2205396917682f723
ashishamodkar <56507158+ashishamodkar@users.noreply.github.com>
2021-02-06 14:07:29 +0530

    1 Excersice to take 2 number from user add Add

---
+++ Excersice_1.py
@@ -0,0 +1,8 @@
+# 1 Excersice to take 2 number from user add Add
+
+print("Enter a Number")
+firstnum = input()
+print("Enter Second Number")
+seconum = input()
+
+print( " Sum Of two Numbers is "int(firstnum)+int(seconum))
