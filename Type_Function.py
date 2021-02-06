b820a31e628d57917f34271a9c09dc024ea06167
ashishamodkar <56507158+ashishamodkar@users.noreply.github.com>
2021-02-06 13:19:45 +0530

    program to type of input and addition no.

---
+++ Type_Function.py
@@ -0,0 +1,13 @@
+
+# Write the program to know type of input and add two string and number
+a = "Ashish "
+b = "Amodkar"
+c = 64
+d = 57.8
+
+print (type(a) , type(b ), type( c ), type(d ))
+print(a+b)
+print(c+d)
+
+
+print("We Can't Write do ADDITION Between STRING And NUMBER")
