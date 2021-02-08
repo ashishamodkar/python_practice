ad5b75005057c2848d07c5a55ca087e4c8c0f432
ashishamodkar <56507158+ashishamodkar@users.noreply.github.com>
2021-02-08 12:25:42 +0530

    String Sliscing Consecpt

---
+++ String_Slicing.py
@@ -0,0 +1,24 @@
+mystr = "Ashish Nitin Amodkar"
+print("String is", mystr)
+
+# its a 5th Charactor of string
+
+print((mystr[5]))
+
+# its a chartor between 6 to 12
+
+print((mystr[6:12]))
+
+# its a function to show length of String
+print(len(mystr))
+
+# Extended Sliscing Or Advance Sliscing
+
+print(mystr[::1])
+# to avoi  to print every 2 charactor
+
+print(mystr[::2])
+
+# to print string in reverce
+# it will  avoid 1 charactor because in python count start from '0'
+print(mystr[::-2])
