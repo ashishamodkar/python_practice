a867fb4a0b5ccd72ec02475130146f880bac1da2
ashishamodkar <56507158+ashishamodkar@users.noreply.github.com>
2021-02-08 13:15:06 +0530

    Some Of String FunctionsExplained

---
+++ String_fumction.py
@@ -0,0 +1,37 @@
+
+mystr = "Ashish Amodkar IS programmer"
+print("String is ", mystr)
+
+# check how string is ended with argument which added in program
+print(mystr.endswith("programmer"))
+print(mystr.endswith("Amodkar"))
+
+# To Count spesific caharactor fro string we use count function
+
+print(mystr.count("m"))
+print(mystr.count("a"))
+print(mystr.count("A"))
+
+
+# Convert 1 st letter in capital case
+print(mystr.capitalize())
+
+# find to find anything from string
+
+print(mystr.find("IS"))
+
+# lower is use to conver all string in lower case
+print(mystr.lower())
+
+# uppercase is use to conver all string in Uppercase
+print(mystr.upper())
+
+# replase anything from string
+print(mystr.replace("Ashish","Soun"))
+
+# to write centred string
+print(mystr.center(1))
+
+# Returns an encoded version of the string
+print(mystr.encode())
+
